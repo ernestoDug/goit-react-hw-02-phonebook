32324b88560db17a6750863af979bdf2213f76fe
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-07-22 13:05:58 +0300

    3(0280)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,36 +5,52 @@ import ContactForm from  './ContactForm'
 import  css from './ContactForm/ContactForm.module.css'
 
 
-
-
  export class App extends Component {
   state = {
     contacts: [],
-    name: ''
+    name: '',
+    number: ''
+
   }
 
 // ***************
 
-//  // Відповідає за оновлення стану
-//  changer = event => {
-//   this.setState({ login: event.target.value });
-// };
+//  //  оновлення 
+ changer = event => {
+  const {name, value} = event.currentTarget;
+  // console.log( event.currentTarget.value)
+  // зміна имені без залежності від старого
+  // для одного обробчика на всих
+this.setState ({[name]: value})
+
+
+};
 
 // // Викликається під час відправлення форми
-// submiter = event => {
-//   event.preventDefault();
-//   console.log(`Signed up as: ${this.state.name}`);
+submiter = event => {
+  event.preventDefault();
+  console.log(`Signed up as: ${this.state.name}`);
 
 //   // Проп, який передається формі для виклику під час сабміту
-//   this.props.onSubmit({ ...this.state });
+  // this.props.onSubmit({ ...this.state });
 // };
 // ***************
+};
  
   render() {
     return (
 <div className={css.forms}>
   <h1>Телефонна книга</h1>
-  <ContactForm/>
+  <ContactForm
+  // значення залежіть від стану
+  names = {this.state.name}
+  number={this.state.number}
+  // меод який змінює 
+  changer={ this.changer}
+  // onChange={this.changer}
+  // submiter = { this.submiter}
+  
+  />
   <h2>Контакти</h2>
   {/* <Filter/> */}
   {/* <ContactList /> */}
@@ -43,7 +59,7 @@ import  css from './ContactForm/ContactForm.module.css'
     );
   }
 }
-
+ 
 
 
            
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,25 +5,45 @@
 import  css from './ContactForm.module.css'
 
 
-const ContactForm = () => 
+const ContactForm = ({names, changer, number}) => 
 {
     return (
-    <form  className={css.formsWr }>
+    <form  
+    className={css.formsWr }>
     {/* onSubmit={this.handleSubmit} */}
     <label className={css.label}>
       Ім'я
       <input 
        className={css.input}
-    // const { login } = this.state;
-
-        type="text"
+          onChange = { changer }
+          names = { names }
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          type="text"
         placeholder="Введіть ім'я"
-        // value={login}
-        // onChange={this.handleChange}
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        name="name"
+        required
       />
     </label>
 
-    <button className={css.buttons} type="submit">Додати контакт </button>
+    <label className={css.label}>
+Номер телефону
+      <input
+           className={css.input}
+  type="tel"
+  placeholder="Введіть номер телефону"
+  onChange = { changer }
+          number = { number }
+  name="number"
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+/>
+     
+    </label>
+
+
+       <button className={css.buttons} type="submit">Додати контакт </button>
     {/* {login} */}
   </form>)
   }
